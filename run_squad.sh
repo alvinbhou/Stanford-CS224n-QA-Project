0adb1be563ec5f2ca0e5b27eba67856877fbb55a
fangihsiao <fyhsiao25@gmail.com>
2020-02-25 03:03:49 -0800

    add bert base training script

---
+++ run_squad.sh
@@ -0,0 +1,19 @@
+python run_squad.py \
+  --name bert-test-1 \
+  --model_type bert \
+  --model_name_or_path bert-base-uncased \
+  --do_train \
+  --do_eval \
+  --do_lower_case \
+  --train_file data/train-v2.0.json \
+  --predict_file data/dev-v2.0.json \
+  --per_gpu_train_batch_size 8 \
+  --learning_rate 3e-5 \
+  --num_train_epochs 2.0 \
+  --max_seq_length 384 \
+  --doc_stride 128 \
+  --version_2_with_negative \
+  --evaluate_during_training \
+  --save_best_only \
+  --logging_steps 2000 \
+  --save_steps 2000
